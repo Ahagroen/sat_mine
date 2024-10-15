20b6f605842b93b640a5142b141283eedf0fbe5b
agro <xandergroen@gmail.com>
2024-10-15 20:53:05 +0000

    Build skeleton of satellite creation system

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,20 @@ mod pass_list;
 
 pub use crate::pass_list::UpcomingPasses;
 pub use crate::ground_track::GroundTrack;
+
+pub use sky_track;
+use sky_track::Satellite;
+
+enum TleSource{
+    Celestrak
+    //Future sources can be implemented here (including credentials if needed)
+}
+
+
+pub fn create_satellite_from_name(name:String,source:TleSource)->Satellite{
+    todo!()
+}
+
+fn call_celestrak()->String{
+    todo!()
+}
